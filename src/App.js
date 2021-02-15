dc2477f16aa75ff8925e67b9651a4a1166985e05
prodgers1 <PaulRodgers1@PaulsMac.local>
2021-02-14 21:34:52 -0700

    flow tutorial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { AuthCluster } from './auth-cluster';
+
+function App() {
+  return (
+    <div className="App">
+      <AuthCluster />
+    </div>
+  );
+}
+
+export default App;
